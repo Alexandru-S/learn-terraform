66461e8c57b7b1cdf7bdeb0ee824f91788b93127
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-05-25 19:37:06 +0200

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "alex-sulea-terra-org-test1"
 
     workspaces {
       name = "learn-terraform"
     }
   }
+  */
 
   required_providers {
     aws = {
